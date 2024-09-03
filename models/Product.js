91c3e077df598e62eaf0fc738efd31a7c8c5dcfb
Moaz Ali <151751659+40moaz@users.noreply.github.com>
2024-09-03 22:09:11 +0200

    cdd

--- models/Product.js
+++ models/Product.js
@@ -1,16 +1,15 @@
-const mongoose = require("mongoose");
+const mongoose = require( "mongoose" );
 const Schema = mongoose.Schema;
 
-const productSchema = new Schema({
+const productSchema = new Schema( {
     title: String,
     description: String,
     price: Number,
     stock: Number,
     brand: String,
     category: String,
-    thumbnail: String,
-    images: [String]
-});
+    image: String,
+} );
 
-const Product = mongoose.model("Product", productSchema);
+const Product = mongoose.model( "Product", productSchema );
 module.exports = Product;
--- routes/products.js
+++ routes/products.js
@@ -1,27 +1,32 @@
-const express = require('express');
+const express = require( 'express' );
 const router = express.Router();
-const Product = require('../models/Product');
+const Product = require( '../models/Product' );
 
 // 1- get Products
-router.get("/", async (req, res) => {
-    try {
+router.get( "/", async ( req, res ) =>
+{
+    try
+    {
         const products = await Product.find();
-        res.json({
+        res.json( {
             message: "The Products have been fetched successfully",
             products: products
-        });
-    } catch (error) {
-        res.status(500).json({
+        } );
+    } catch ( error )
+    {
+        res.status( 500 ).json( {
             message: "An error occurred",
             error: error.message
-        });
+        } );
     }
-});
+} );
 
 // 2- post Product
-router.post("/", async (req, res) => {
-    try {
-        const newProduct = new Product({
+router.post( "/", async ( req, res ) =>
+{
+    try
+    {
+        const newProduct = new Product( {
             title: req.body.title,
             description: req.body.description,
             price: req.body.price,
@@ -30,41 +35,47 @@ router.post("/", async (req, res) => {
             category: req.body.category,
             thumbnail: req.body.thumbnail,
             images: req.body.images
-        });
+        } );
 
         await newProduct.save();
 
-        res.json({
+        res.json( {
             message: "The Product was created successfully",
             product: newProduct
-        });
-    } catch (error) {
-        res.status(500).json({
+        } );
+    } catch ( error )
+    {
+        res.status( 500 ).json( {
             message: "An error occurred",
             error: error.message
-        });
+        } );
     }
-});
+} );
 
 // 3- get one Product
-router.get("/:id", async (req, res) => {
-    try {
-        const oneProduct = await Product.findById(req.params.id);
-        res.json({
+router.get( "/:id", async ( req, res ) =>
+{
+    try
+    {
+        const oneProduct = await Product.findById( req.params.id );
+        res.json( {
             message: "The Product has been fetched successfully",
             product: oneProduct
-        });
-    } catch (error) {
-        res.status(500).json({
+        } );
+    } catch ( error )
+    {
+        res.status( 500 ).json( {
             message: "An error occurred",
             error: error.message
-        });
+        } );
     }
-});
+} );
 
 // 4- edit one Product 
-router.put("/:id", async (req, res) => {
-    try {
+router.put( "/:id", async ( req, res ) =>
+{
+    try
+    {
         const updatedProduct = await Product.findByIdAndUpdate(
             req.params.id,
             {
@@ -80,37 +91,42 @@ router.put("/:id", async (req, res) => {
             { new: true } // This option returns the updated document
         );
 
-        if (!updatedProduct) {
-            return res.status(404).json({
+        if ( !updatedProduct )
+        {
+            return res.status( 404 ).json( {
                 message: "Product not found"
-            });
+            } );
         }
 
-        res.json({
+        res.json( {
             message: "The product updated successfully",
             product: updatedProduct
-        });
-    } catch (error) {
-        res.status(500).json({
+        } );
+    } catch ( error )
+    {
+        res.status( 500 ).json( {
             message: "An error occurred",
             error: error.message
-        });
+        } );
     }
-});
+} );
 // 5- delete Product
-router.delete("/:id", async (req, res) => {
-    try {
-        const oneProduct = await Product.findByIdAndDelete(req.params.id);
-        res.json({
+router.delete( "/:id", async ( req, res ) =>
+{
+    try
+    {
+        const oneProduct = await Product.findByIdAndDelete( req.params.id );
+        res.json( {
             message: "The Product has been deleted successfully",
             product: oneProduct
-        });
-    } catch (error) {
-        res.status(500).json({
+        } );
+    } catch ( error )
+    {
+        res.status( 500 ).json( {
             message: "An error occurred",
             error: error.message
-        });
+        } );
     }
-});
+} );
 
 module.exports = router;
