08a0776f1cd29b17cd136aa7caf3f5d23cdba2c4
Moaz Ali <151751659+40moaz@users.noreply.github.com>
2024-10-23 07:33:44 +0200

    Update Article.js

--- models/Article.js
+++ models/Article.js
@@ -4,8 +4,16 @@ const Schema = mongoose.Schema;
 const ArticleSchema = new Schema( {
     title: String,
     body: String,
+    cover: String,
     date: Number,
-    author_id: Number
+    author_id: Number,
+    comments: [
+        {
+            user_id: String,
+            comment_body: String,
+            date: String
+        }
+    ]
 } );
 
 const Article = mongoose.model( "Article", ArticleSchema );
